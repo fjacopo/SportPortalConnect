25c004f1bed70ba052d4efd6402c15774b1d465a
fjacopo <101565095+fjacopo@users.noreply.github.com>
2024-05-03 19:13:48 +0200

    Add files via upload

--- sport_portal_db.sql
+++ sport_portal_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 03, 2024 alle 18:07
+-- Creato il: Mag 03, 2024 alle 19:07
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -39,12 +39,32 @@ CREATE TABLE `richieste_giocatori` (
   `stato` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dump dei dati per la tabella `richieste_giocatori`
+-- Struttura della tabella `users`
 --
 
-INSERT INTO `richieste_giocatori` (`id`, `nome`, `cognome`, `data_nascita`, `email`, `username`, `ruolo`, `cod_squadra`, `stato`) VALUES
-(3, 'Luca', 'Puma', '1983-06-11', 'lucapuma@gmail.com', 'lucapuma', NULL, 'GYH53FMNXY', 0);
+CREATE TABLE `users` (
+  `nome` varchar(255) DEFAULT NULL,
+  `cognome` varchar(255) DEFAULT NULL,
+  `data_nascita` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `ruolo` varchar(255) DEFAULT NULL,
+  `cod_squadra` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`nome`, `cognome`, `data_nascita`, `email`, `username`, `password`, `ruolo`, `cod_squadra`) VALUES
+('Allenatore', 'A', '2019-04-30', 'allenatore@gmail.com', 'allenatore', '$2y$10$pvVxgTrau0EhgqKqeA1l3O1O1vLIfmDHPvhN.O0fbXWojtLapUaiK', 'Allenatore', 'oM4F3yS0VZ'),
+('Domenico', 'Manca', '2005-06-25', 'domymanca.ciao@gmail.com', 'DomeManca', '$2y$10$xcnXwU8BHXrWQHA.RhOz9e26cR6MVI56/sZ1ll72YpEhsZZftb9iW', NULL, NULL),
+('Jacopo', 'Ferrari', '2005-04-20', 'fjacopo10@gmail.com', 'ferrarijacopo', '$2y$10$Cdn0dNTaR.2L0mcbwMPK8e4DL75FcgP4hUzHX5wL/qfOCOrMfhXOC', 'Allenatore', 'GYH53FMNXY'),
+('Viola', 'Ferrari', '2000-05-14', 'vio00@gmail.com', 'vio', '$2y$10$2r5KZ6hwO5TseLB/nmkAVurbYA3gA1dw3W/5NMF1l59riyDNYXiCq', 'Preparatore', 'GYH53FMNXY');
 
 --
 -- Indici per le tabelle scaricate
@@ -58,6 +78,14 @@ ALTER TABLE `richieste_giocatori`
   ADD KEY `username` (`username`);
 
 --
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
@@ -65,7 +93,7 @@ ALTER TABLE `richieste_giocatori`
 -- AUTO_INCREMENT per la tabella `richieste_giocatori`
 --
 ALTER TABLE `richieste_giocatori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22129;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22135;
 
 --
 -- Limiti per le tabelle scaricate
