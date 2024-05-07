8f1da6c3e701d07cb3acae896f3c809300af1e54
fjacopo <101565095+fjacopo@users.noreply.github.com>
2024-05-07 11:34:10 +0200

    Add files via upload

--- sport_portal_db.sql
+++ sport_portal_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 06, 2024 alle 22:17
+-- Creato il: Mag 07, 2024 alle 11:33
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -58,7 +58,7 @@ CREATE TABLE `tbl_events` (
 --
 
 INSERT INTO `tbl_events` (`id`, `title`, `start`, `end`, `location`) VALUES
-(24, 'allenamento ', '2024-05-07 18:30:00', '2024-05-07 21:24:00', 'campo sportivo');
+(31, 'bho', '2024-05-09 11:13:00', '2024-05-10 11:13:00', '');
 
 -- --------------------------------------------------------
 
@@ -83,7 +83,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`nome`, `cognome`, `data_nascita`, `email`, `username`, `password`, `ruolo`, `cod_squadra`) VALUES
 ('a', 'a', '2019-05-04', 'aa@mail.com', 'a', '$2y$10$4q4IZ.QEeV68kmFQb8lQXOeM6DJtMlY/D6PzyUMlyqJKVeHk.rSY6', 'giocatore', 'GYH53FMNXY'),
-('Domenico', 'Manca', '2005-06-25', 'domymanca.ciao@gmail.com', 'DomeManca', '$2y$10$9yz9Yc8.FfyYMsu4RnaAS.3moi5X.cGRYpVz0flpbuShaz8ofeYBi', 'preparatore', 'GYH53FMNXY'),
+('Domenico', 'Manca', '2005-06-25', 'domymanca.ciao@gmail.com', 'DomeManca', '$2y$10$Z5rjwko438LnVmEoU6OG/OQDJzWqcMkEw6h9.bbn5w/hYCN7z9Pza', 'preparatore', 'GYH53FMNXY'),
 ('Jacopo', 'Ferrari', '2005-04-20', 'fjacopo10@gmail.com', 'ferrarijacopo', '$2y$10$fXUk3gUdC0ezOZQQDGO.u.hWeYkHvWkUXodzbbolD.gJBGrzlIhiW', 'Allenatore', 'GYH53FMNXY'),
 ('prova', 'prova', '2019-05-01', 'prova@gmail.com', 'prova', '$2y$10$scx4MV64BkhpRipzQfTdlOGqumpX6.3VAl7aArx3qff6UJXicawRC', NULL, NULL);
 
@@ -126,7 +126,7 @@ ALTER TABLE `richieste_giocatori`
 -- AUTO_INCREMENT per la tabella `tbl_events`
 --
 ALTER TABLE `tbl_events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Limiti per le tabelle scaricate
